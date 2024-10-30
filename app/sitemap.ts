1593592236fc9f63fae027a6dc2ddfa86dfdd657
Paul Grau <paul@graycoding.com>
2024-10-30 13:27:10 +0200

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,11 +4,11 @@ export const baseUrl = "https://graycoding.com";
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+    url: `${baseUrl}/post/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
 
-  let routes = ["", "/blog"].map((route) => ({
+  let routes = ["", "/post"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
