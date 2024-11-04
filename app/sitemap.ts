7ba0e4e2ef9ebc2d173a5ac1ede377225beae294
Paul Grau <paul@graycoding.com>
2024-11-04 11:02:38 +0200

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,7 +8,7 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }));
 
-  let routes = ["", "/post"].map((route) => ({
+  let routes = ["", "/tag/ui", "/tag/global", "/tag/product"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
